5f7a1cc58854f1f6c6676033a29a88918fbf4298
PipPupNub <144836800+MironLevchenko@users.noreply.github.com>
2023-12-20 15:13:37 +0300

    Попытка не пытка 8 лаба

---
+++ labs/Lab_08_events_js/solution/task4/app.js
@@ -0,0 +1,49 @@
+window.onload = function(){
+    document.getElementById('Green').addEventListener('input', function() {
+        // Если поле input не пустое, изменяем цвет блока на зеленый
+        if (this.value != "") {
+            document.getElementById('blockColor').style.backgroundColor = 'green';
+        } else {
+            // Если поле input пустое, возвращаем блоку исходный цвет
+            document.getElementById('blockColor').style.backgroundColor = 'white';
+        }
+   });
+   document.getElementById('Red').addEventListener('input', function() {
+        // Если поле input не пустое, изменяем цвет блока на красный
+        if (this.value != "") {
+            document.getElementById('blockColor').style.backgroundColor = 'red';
+        } else {
+            // Если поле input пустое, возвращаем блоку исходный цвет
+            document.getElementById('blockColor').style.backgroundColor = 'white';
+        }
+    });
+    document.getElementById('Blue').addEventListener('input', function() {
+        // Если поле input не пустое, изменяем цвет блока на синий
+        if (this.value != "") {
+            document.getElementById('blockColor').style.backgroundColor = 'blue';
+        } else {
+            // Если поле input пустое, возвращаем блоку исходный цвет
+            document.getElementById('blockColor').style.backgroundColor = 'white';
+        }
+    });
+    let count =0;
+    document.getElementById('button').onclick = function(){
+        let miniBlock = document.createElement('div');
+        miniBlock.id ='miniBlock';
+        miniBlock.class="element";
+        miniBlock.innerText = 'Я цветной';
+        document.getElementById('ParentBlock').appendChild(miniBlock);
+        count++;
+        if(count<15){
+            miniBlock.style.cssText = `height:200px; width:20%; background: blue;position:absolute;`;
+        }else{
+            miniBlock.style.cssText = `height:200px; width:20%; background: brown;position:absolute;`;
+        }
+    }
+    document.getElementById('miniBlock').addEventListener('div',function() {
+        let color =  getComputedStyle(this).backgroundColor;
+        console.log(color)
+        document.body.style.backgroundColor = color;
+    });
+};
+
